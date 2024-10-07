1658f8d2ce08d92ac1509050f6908d7a9bc1df4e
alizadehf-laya <145831251+alizadehf-laya@users.noreply.github.com>
2024-10-07 02:39:01 -0700

    another example for map added

--- src/Body/Map.jsx
+++ src/Body/Map.jsx
@@ -1,13 +1,22 @@
 const Map=()=>{
-
-  const Names=["laya","rosy","orkideh","helia"]
+//   const Names=["laya","rosy","orkideh","helia"]
+const users=[
+            {name:"laya" ,age:36},
+            {name:"rossy" ,age:12},
+            {name:"orkideh" ,age:3},
+            {name:"yasser" ,age:42}
+            ]
     return(
         <>
-  {Names.map((name,index)=>{
+        {users.map((user,index)=>{
+            return  <h4 key={index}>{user.name}:{user.age}</h4>
+           
+        })}
+  {/* {Names.map((name,index)=>{
     return(
         <h4 key={index}>{name}</h4>
     )
-  })}
+  })} */}
         </>
     )
 }
